fe7e4b7e56dc98afde3941ff9064b6c1816bb5c0
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-19 20:44:57 -0500

    reading api route

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const notes = require('./db/db.json')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
 // The following HTML routes should be created:
 // GET /notes should return the notes.html file.
 // GET * should return the index.html file.
@@ -17,7 +13,7 @@ app.use(express.json());
 //API routes 
 // GET /api/notes 
 // read the db.json file and return all saved notes as JSON.
-app.get('/api/notes', (req, res) => res.json(notes))
+app.get('/api/notes', (req, res) => res.json(notes)); 
 
 // POST /api/notes should receive a new note to save on the request body, 
 //add it to the db.json file, and 
@@ -25,14 +21,13 @@ app.get('/api/notes', (req, res) => res.json(notes))
 //You'll need to find a way to give each note a unique id when it's saved 
 //(look into npm packages that could do this for you).
 
-// app.post('api/notes', (req, res) => {
-//   const newNote = req.body; 
-//   tableArray.push(newNote);
-//   res.json(db);
-// })
+app.post('api/notes', (req, res) => {
+  const newNote = req.body; 
+  db.push(newNote);
+  res.json(db);
+})
 
 // Starting my server
 app.listen(PORT, () => {
-  //console.log(`App listening on PORT: ${PORT}`)
-  console.log(notes);
+  console.log(`App listening on PORT: ${PORT}`)
 });
