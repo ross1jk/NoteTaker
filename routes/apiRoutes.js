198f2d95cbdc875b2386b045b168e6838f087162
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-21 20:51:50 -0500

    updates made to server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,27 @@
 //API routes 
 const notes = require('../db/db.json');
-const fs = require('fs'); 
+const fs = require('fs');
+const util = require('util');
+// const router = reauire('express').Router();
+// const store = require('../db/store')
 
 module.exports = (app) => {
-
 // GET /api/notes 
 // read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => res.json(notes)); 
+
 // POST /api/notes should receive a new note to save on the request body, 
-//add it to the db.json file, and 
-//then return the new note to the client. 
-//You'll need to find a way to give each note a unique id when it's saved 
-//(look into npm packages that could do this for you).
+// add it to the db.json file, and 
+// then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved 
+// (look into npm packages that could do this for you).
 
 app.post('api/notes', (req, res) => {
+  const savedNotes = JSON.parse(notes);
   const newNote = req.body; 
-  notes.push(newNote);
+  savedNotes.push(newNote); 
+  fs.appendFile('../db/db.json', JSON.stringify(savedNotes)); 
+  console.log("you did something jac");
   res.json(notes);
 }); 
 }; 
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes') 
+// const htmlRoutes = require('./routes/htmlRoutes') 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// this is sending my files 
+app.use(express.static(__dirname + '/public'));
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app); 
+
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 // Starting my server
 app.listen(PORT, () => {
