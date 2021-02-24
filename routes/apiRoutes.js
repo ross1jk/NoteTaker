8f85bbc19e0617815381a55620ea989d00663ccd
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-23 19:09:28 -0500

    unique ID is applying for each notes when they are created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,24 +2,33 @@
 const notes = require('../db/db.json');
 const fs = require('fs');
 const { finished } = require('stream');
-var uniqid = require('uniqid');
+const shortid = require('shortid');
 
 module.exports = (app) => {
 
   app.get('/api/notes', (req, res) => res.json(notes));
 
   app.post('/api/notes', (req, res) => {
-      // unique id 
-    notes.push(req.body); 
-    let update = JSON.stringify(notes, null, 2); 
+    // unique id
+    req.id = shortid.generate();
+    const newNote = { 
+      title: req.body.title, 
+      text: req.body.text,
+      id: req.id
+    };
+    notes.push(newNote);
+    let update = JSON.stringify(notes, null, 2);
     fs.writeFile('./db/db.json', update, finshed);
-      function finshed() {
-        console.log("Updated Notes");
-      };
-     res.end();
+    function finshed() {
+      console.log("Updated Notes");
+    };
+    res.end();
   });
 
   // Selected Note Display
 
   // Delete Note   
 };
+
+
+
